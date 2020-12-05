62c8a1a00a176f9f6e4b86e358d714e16b2121cb
Natashanadya <72211516+Natashanadya@users.noreply.github.com>
2020-12-05 16:41:06 +0700

    Delete module-info.java

--- SistemCatatPoin/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.sistemcatatpoin {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.sistemcatatpoin to javafx.fxml;
-    exports com.mycompany.sistemcatatpoin;
-}
